b6eb24834c194a1608b87eab20f761da3d0af5be
Vinícius Chagas Soares <vcsoares@inf.ufrgs.br>
2018-05-13 01:16:01 -0300

    refactoring + placeholder stuff

--- include/FileHandler.hpp
+++ include/FileHandler.hpp
@@ -1,17 +1,18 @@
 #include "bropdoxUtil.hpp"
 
 class FileHandler {
-    bool write_file(char const* file_name, data_buffer_t* file_data[]);
 
-    data_buffer_t* get_file(char const* file_name);
-
-    // TODO: some function that gets all modified files
-    std::vector<data_buffer_t*> get_file_list(std::vector<char const*> file_list);
 private:
     std::string id;
 
     bool init_client(std::string client_id);
 public:
     FileHandler(std::string client_id, bool init);
-    //
+
+    //eu não sei se é public ou private alguém bota no lugar certo depois
+    bool write_file(char const* file_name, data_buffer_t* file_data[]);
+    data_buffer_t* get_file(char const* file_name);
+
+    // TODO: some function that gets all modified files
+    std::vector<data_buffer_t*> get_file_list(std::vector<char const*> file_list);
 };
--- src/FileHandler.cpp
+++ src/FileHandler.cpp
@@ -1,16 +1,33 @@
 #include "../include/FileHandler.hpp"
 //Function that receives an *databuffer and a filename and writes on disk
 
-bool write_file(char const* file_name, data_buffer_t* file_data[])
+bool FileHandler::write_file(char const* file_name, data_buffer_t* file_data[])
 {
-    ofstream myFile;
-    myFile.open("~/" + file_name, ios::binary | ios::in | ios::out);
+    std::ofstream myFile;
+    myFile.open("~/" + *file_name, std::ios::binary | std::ios::in | std::ios::out);
 
-    for (auto const& item : file_data){
+    for (auto const& item : **file_data){
         myFile << item;
     }   
 
     myFile.close();
 
     return true;
-}   
+}
+
+bool FileHandler::init_client(std::string client_id) {
+    return false;
+}
+
+FileHandler::FileHandler(std::string client_id, bool init) {
+
+}
+
+data_buffer_t *FileHandler::get_file(char const *file_name) {
+    return nullptr;
+}
+
+std::vector<data_buffer_t *> FileHandler::get_file_list(std::vector<char const *> file_list) {
+    return std::vector<data_buffer_t *>();
+}
+
